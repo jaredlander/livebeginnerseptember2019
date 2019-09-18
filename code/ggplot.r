2628e52cefab8098f2f25f96fa7219360bb1070d
Jared Lander <info@jaredlander.com>
2019-09-18 16:41:34 -0400

    Showed densities

--- code/ggplot.r
+++ code/ggplot.r
@@ -39,3 +39,55 @@ ggplot(diamonds, aes(x=carat, y=price, color=cut)) +
 ggplot(diamonds, aes(x=carat, y=price)) + 
     geom_point(shape=1, size=1, aes(color=cut)) + 
     geom_smooth()
+ggplot(diamonds, aes(x=carat, y=price)) + 
+    geom_point(shape=1, size=1, aes(color=cut)) + 
+    geom_smooth(aes(color=cut)) + 
+    geom_smooth(color='hotpink')
+
+ggplot(diamonds, aes(x=carat, y=price)) + 
+    geom_point(shape=1, size=1, aes(color=cut)) + 
+    geom_smooth(aes(color=cut)) + 
+    geom_smooth(color='hotpink') +
+    facet_wrap( ~ cut )
+
+ggplot(diamonds, aes(x=carat, y=price)) + 
+    geom_point(shape=1, size=1, aes(color=cut)) + 
+    geom_smooth(aes(color=cut)) + 
+    geom_smooth(color='hotpink') +
+    facet_wrap( ~ cut ) +
+    theme(legend.position='none')
+
+ggplot(diamonds, aes(x=carat, y=price)) + 
+    geom_point(shape=1, size=1, aes(color=cut), alpha=1/3) + 
+    geom_smooth(aes(color=cut)) + 
+    geom_smooth(color='red') +
+    facet_wrap( ~ cut ) +
+    theme(legend.position='none')
+
+ggplot(diamonds, aes(x=price)) + 
+    geom_histogram()
+
+ggplot(diamonds, aes(x=price, color=cut)) + 
+    geom_histogram()
+ggplot(diamonds, aes(x=price, fill=cut)) + 
+    geom_histogram()
+ggplot(diamonds, aes(x=price, fill=cut)) + 
+    geom_histogram(position=position_dodge())
+
+ggplot(diamonds, aes(x=price, fill=cut)) + 
+    geom_density()
+ggplot(diamonds, aes(x=price, fill=cut)) + 
+    geom_density(alpha=1/3)
+
+ggplot(diamonds, aes(x=price, fill=cut)) + 
+    geom_density() +
+    facet_wrap( ~ cut ) + 
+    theme(legend.position='none')
+
+library(ggridges)
+ggplot(diamonds, aes(x=price, y=cut)) + 
+    geom_density_ridges()
+ggplot(diamonds, aes(x=price, y=cut, fill=cut)) + 
+    geom_density_ridges() + 
+    theme(legend.position='none')
+
